ff34ac52c98e02c80c6cf3a7a7426b36473a1277
Devdeep <devdeep120205@gmail.com>
2025-09-02 22:06:27 +0530

    changes

--- career/src/main.jsx
+++ career/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ReactGA from 'react-ga4'; // Import the library
-import { HelmetProvider } from 'react-helmet-async'; // Import
+
 import App from './App.jsx';
 import './index.css';
 import './i18n';
@@ -11,8 +11,8 @@ ReactGA.initialize("G-54RKL82Q5C"); // Paste your Measurement ID here
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
+    
     <App />
-    </HelmetProvider>
+    
   </React.StrictMode>,
 );
